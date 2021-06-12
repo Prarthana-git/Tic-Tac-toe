138645af58012cc363b7eb865492ad9dcbba5702
Prarthana-git <prarthanachaudhari@gmail.com>
2021-06-12 10:05:45 +0530

    added user input

--- src/com/bridgelabz/TicTacToe/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe/TicTacToe.java
@@ -1,19 +1,38 @@
 package com.bridgelabz.TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
-	public static char[] createBoard() { // created board of size 10
-		char[] board = new char[10];
-		for (int i = 1; i < board.length; i++) {
+	private  char[] board = new char[10];
+	public  void createBoard() { // created board of size 10
+		for (int i = 1; i < 9; i++) {
 			// assign empty space to each element and start it from position 1
 			board[i] = ' ';
 		}
-		return board;
 	}
-
+ //make choice for x or o
+	public static void makeChoice()
+	{
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter What You Want X or O");
+		char player=obj.next().charAt(0);
+		char computer=' ';
+		if(player=='X'||player=='x')
+		{
+			computer='O';
+		}else if (player=='O'||player=='o')
+		{
+			computer='X';
+		}else
+		{
+			System.out.println("Invalid Input");
+		}
+	}
 	public static void main(String[] args) {
 		// welcome message
 		System.out.println("Welcome to the TicTacToe game");
-		createBoard();// call the method in main class
+		TicTacToe tictactoe=new TicTacToe();
+		tictactoe.createBoard();// call the method in main class
+		tictactoe.makeChoice();
 	}
-
 }
