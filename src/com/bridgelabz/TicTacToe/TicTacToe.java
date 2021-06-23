3cd8a759758df488e1ae8a9d4a291d42a9c11594
Prarthana-git <prarthanachaudhari@gmail.com>
2021-06-23 13:47:01 +0530

    Added Win tie and lose

--- src/com/bridgelabz/TicTacToe/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe/TicTacToe.java
@@ -30,7 +30,28 @@ public class TicTacToe {
 			System.out.println("Invalid Input");
 		}
 	}
+	public boolean validateMove(int location) {
+		if (0 < location && location < 10) {
+			if (board[location] != ' ') return false;
+			return true;
+		}
+		return false;
+	}
 
+	public void makeUserMove() {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("enter a location number ");
+		int location = obj.nextInt();
+		if (validateMove(location)) {
+			board[location] = userChoice;
+			counter++;
+			checkGameStatus("User");
+		} else {
+			System.out.println("Sorry you have selected the wrong position try again");
+			if(counter<=9) makeUserMove();
+			else return;
+		}
+	}
 	public void showBoard(char[] board) {
 		System.out.println("\n" + board[1] + "|" + board[2] + "|" + board[3]);
 		System.out.println("------");
@@ -51,16 +72,48 @@ public class TicTacToe {
 			}
 		}
 	}
+	public boolean allEqual(int a, int b, int c) {
+		return (a == c) && (b == c);
+	}
+
+	public void checkGameStatus(String key) {
+		displayBoard(board);
+		// System.out.println(counter);
+		if (counter <= 9) {
+			for (int i = 1; i < 10; i = i + 3) {
+				if (allEqual(board[i], board[i + 1], board[i + 2]) && board[i] != ' ') {
+					System.out.println(key + " has won the game");
+					return;
+				}
+			}
+			for (int i = 1; i < 3; i = i + 1) {
+				if (allEqual(board[i], board[i + 3], board[i + 6]) && board[i] != ' ') {
+					System.out.println(key + " has won the game");
+					return;
+				}
+			}
+			if (allEqual(board[1], board[5], board[9]) && board[1] != ' ') {
+				System.out.println(key + " has won the game");
+				return;
+			}
+			if (allEqual(board[3], board[5], board[7]) && board[3] != ' ') {
+				System.out.println(key + " has won the game");
+				return;
+			}
+			if (key.equals("Computer") && counter <9) makeUserMove();
+			else if (key.equals("User") && counter <9) makeComputerMove();
+			else System.out.println("it's a tie");
+		}
+	}
 
 	public static void main(String[] args) {
-		// welcome message
-		System.out.println("Welcome to the TicTacToe game");
-		TicTacToe tictactoe = new TicTacToe();
-		tictactoe.createBoard();// call the method in main class
-		tictactoe.makeChoice();
-		tictactoe.makeMove(board);
-		tictactoe.showBoard(board);
-	
-
+		int gamestatus = 0;
+		TicTacToe game = new TicTacToe();
+		System.out.println("welcome to TikTakToe");
+		char[] board = game.createBoard();
+		game.makeChoice();
+		if (tossToBegin()) game.makeComputerMove();
+		else game.makeUserMove();
 	}
+}}
 }
